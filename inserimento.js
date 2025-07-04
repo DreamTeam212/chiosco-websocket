d0a2cfaf0067aa5681c6c946bc4b9b951926f584
DreamTeam212 <adrianofedeli07@gmail.com>
2025-07-04 16:42:53 +0200

    Add files via upload

---
+++ inserimento.js
@@ -0,0 +1,26 @@
+const ws = new WebSocket(`ws://${location.host}`);
+
+const form = document.getElementById('comandaForm');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  function getMultipleValues(id) {
+    const options = document.getElementById(id).selectedOptions;
+    return Array.from(options).map(opt => opt.value);
+  }
+
+  const comanda = {
+    numero: document.getElementById('numero').value,
+    tipoPiatto: document.getElementById('tipoPiatto').value,
+    carne: getMultipleValues('carne'),
+    extra: getMultipleValues('extra'),
+    verdure: getMultipleValues('verdure'),
+    salse: getMultipleValues('salse'),
+    fritti: getMultipleValues('fritti')
+  };
+
+  ws.send(JSON.stringify({ type: 'new', comanda }));
+
+  form.reset();
+});
