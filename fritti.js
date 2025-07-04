db3b8161e3f48eb3486cbd6ac9e5bbadcccb92d8
DreamTeam212 <adrianofedeli07@gmail.com>
2025-07-04 17:01:06 +0200

    Update fritti.js

--- fritti.js
+++ fritti.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket(`ws://${location.host}`);
+const ws = new WebSocket(`wss://${location.host}`);
 const lista = document.getElementById('listaFritti');
 
 ws.onmessage = function (event) {
